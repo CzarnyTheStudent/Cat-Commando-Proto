2a2ad7f4ebc6c76e3611ffeeafbfa6d0caf6c365
CzarnyTheStudent <kpabiniak@edu.cdv.pl>
2022-11-18 18:44:24 +0100

    upd 2.3 (kursor w grze)

---
+++ Assets/Scripts/CursorManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    [SerializeField] private Texture2D cursorTexture;
+
+    private Vector2 cursorHotspot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cursorHotspot = new Vector2(cursorTexture.width / 2, cursorTexture.height / 2);
+        Cursor.SetCursor(cursorTexture, cursorHotspot, CursorMode.Auto);
+    }
+
+    
+}
